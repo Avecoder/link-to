972ec1d6e8fd9089f20d0e0df1aeaff564e01162
Avecoder <foravecode@gmail.com>
2023-09-02 07:23:56 +0300

    fix bugs

--- src/components/GoogleButton.jsx
+++ src/components/GoogleButton.jsx
@@ -9,7 +9,7 @@ const GoogleButton = ({children, onClick}) => {
             whileTap={{ scale: 0.9 }}
             className="shadow-md rounded-md bg-black text-white flex px-4 py-2.5 items-center w-fit gap-x-5"
         >
-            <img src="./src/assets/images/Google.svg" alt="google" />
+            <img src="./google.svg" alt="google" />
             <span>{children}</span>
         </motion.button>
     )
